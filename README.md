acc35f0e3791a54600fd6c383455e6e47e2f9fa0
matheusrenner22 <matheusrennerdev@outlook.com>
2024-04-24 05:03:12 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Calculador-IMC
+
+Um simples calculador de imc feito com HTML, CSS e JavaScript.
+
+### Screenshot
+
+![](./img/screenshot.png)
---
+++ index.js
@@ -0,0 +1,43 @@
+function executaPrograma() {
+    const submit = document.querySelector('button');
+
+    function executaLogica(event) {
+        event.preventDefault();
+
+        const peso = document.querySelector('form .peso').value;
+        const altura = document.querySelector('form .altura').value;
+        const calculoImc = peso / (altura * altura);
+        const resultado = document.querySelector('.resultado');
+
+        if (calculoImc < 18.5) {
+            resultado.classList.remove('desativado');
+            resultado.classList.add('ativado');
+            resultado.innerHTML = `IMC: ${calculoImc.toFixed(2)} <span>(Abaixo do peso)</span>`;
+        } else if (calculoImc >= 18.5 && calculoImc <= 24.9) {
+            resultado.classList.remove('desativado');
+            resultado.classList.add('ativado');
+            resultado.innerHTML = `IMC: ${calculoImc.toFixed(2)} <span>(Peso normal)</span>`;
+        } else if (calculoImc >= 25 && calculoImc <= 29.9) {
+            resultado.classList.remove('desativado');
+            resultado.classList.add('ativado');
+            resultado.innerHTML = `IMC: ${calculoImc.toFixed(2)} (Sobrepeso)`;
+        } else if (calculoImc >= 30 && calculoImc <= 34.9) {
+            resultado.classList.remove('desativado');
+            resultado.classList.add('ativado');
+            resultado.innerHTML = `IMC: ${calculoImc.toFixed(2)} (Obesidade grau 1)`;
+        } else if (calculoImc >= 35 && calculoImc <= 39.9) {
+            resultado.classList.remove('desativado');
+            resultado.classList.add('ativado');
+            resultado.innerHTML = `IMC: ${calculoImc.toFixed(2)} (Obesidade grau 2)`;
+        } else {
+            resultado.classList.remove('desativado');
+            resultado.classList.add('ativado');
+            resultado.innerHTML = `IMC: ${calculoImc.toFixed(2)} (Obesidade grau 3)`;
+        }
+    }
+
+    submit.addEventListener('click', executaLogica);
+
+}
+
+executaPrograma();
